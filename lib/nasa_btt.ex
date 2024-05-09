820c4087165634aaaeee575eae57ba387c4f95df
zastrixarundell <luka@bodroza.com>
2024-05-09 18:42:32 +0200

    Initial commit

---
+++ lib/nasa_btt.ex
@@ -0,0 +1,18 @@
+defmodule NasaBtt do
+  @moduledoc """
+  Documentation for `NasaBtt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NasaBtt.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nasa_btt_test.exs
@@ -0,0 +1,8 @@
+defmodule NasaBttTest do
+  use ExUnit.Case
+  doctest NasaBtt
+
+  test "greets the world" do
+    assert NasaBtt.hello() == :world
+  end
+end
